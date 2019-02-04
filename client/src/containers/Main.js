49ff561178ff09151ced6fb85d402e5da86f805c
adamGuinea <adamguinea22@gmail.com>
2019-02-04 21:53:06 +1300

    taking care of error handling on forms

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
-import Homepage from '../components/Homepage'
-import AuthForm from '../components/AuthForm'
+import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
+import {authUser} from '../store/actions/auth';
+import {removeError} from '../store/actions/errors';
 
 const Main = props => {
+    const {authUser, errors, removeError} = props;
     return (
         <div className="container">
             <Switch>
@@ -13,15 +16,27 @@ const Main = props => {
                     path='/signin' 
                     render={props => {
                         return (
-                            <AuthForm buttonText='Log in' heading='Welcome back' {...props}/>
+                            <AuthForm 
+                                removeError={removeError}
+                                errors={errors}
+                                onAuth={authUser} 
+                                buttonText='Log in' 
+                                heading='Welcome back' 
+                                {...props}
+                            />
                         ) 
                     }} 
                 />
-                <Route exact 
+                <Route 
+                    exact 
                     path='/signup' 
                     render={props => {
                         return (
                             <AuthForm 
+                                removeError={removeError}
+                                errors={errors}
+                                onAuth={authUser}
+                                signUp
                                 buttonText='Sign me up!' 
                                 heading='Join Chatta today' 
                                 {...props}
@@ -37,8 +52,9 @@ const Main = props => {
 function mapStateToProps(state){
 
     return {
-        currentUser: state.currentUser
+        currentUser: state.currentUser,
+        errors: state.errors
     }
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, {authUser, removeError})(Main));
