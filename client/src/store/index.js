d8507c01d0ab3f832863360d49352b79bbfba535
adamGuinea <adamguinea22@gmail.com>
2019-02-04 15:58:05 +1300

    displaying data to client

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,5 +3,13 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 export function configureStore(){
-    const store = createStore(rootReducer)
-}
+    const store = createStore(
+        rootReducer,
+        compose(
+            applyMiddleware(thunk),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
+
+    return store;
+};
