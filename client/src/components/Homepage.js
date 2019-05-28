286890e9239635f3727e59f3fd84d97d57da4d4f
adamGuinea <adamguinea22@gmail.com>
2019-05-28 23:33:06 +1200

    building the tour overview

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Logo from "../images/chatta-logo.png";
 import Person1 from "../images/person-2.jpg";
+import Cycle1 from "../images/cycle-1.jpg";
+import Cycle2 from "../images/cycle-2.jpg";
+import Cycle3 from "../images/cycle-3.jpg";
 import { Link } from "react-router-dom";
 import MessageTimeline from "./MessageTimeline";
 
@@ -82,7 +85,51 @@ const Homepage = ({ currentUser }) => {
             </ul>
             <div className="legal">&copy; 2019 Death Road Tours Bolivia</div>
           </nav>
-          <main className="tour-view">Tour view</main>
+          <main className="tour-view">
+            <div className="gallery">
+              <figure className="gallery__item">
+                <img src={Cycle1} alt="cyclist 1" className="gallery__photo" />
+              </figure>
+              <figure className="gallery__item">
+                <img src={Cycle2} alt="cyclist 2" className="gallery__photo" />
+              </figure>
+              <figure className="gallery__item">
+                <img src={Cycle3} alt="cyclist 3" className="gallery__photo" />
+              </figure>
+            </div>
+            <div className="overview">
+              <h1 className="overview__heading">
+                Cycle the World's Most Dangerous Road
+              </h1>
+              <div className="overview__stars">
+                <svg className="overview__icon-star">
+                  <use xlinkHref="/sprite.svg#icon-star" />
+                </svg>
+                <svg className="overview__icon-star">
+                  <use xlinkHref="/sprite.svg#icon-star" />
+                </svg>
+                <svg className="overview__icon-star">
+                  <use xlinkHref="/sprite.svg#icon-star" />
+                </svg>
+                <svg className="overview__icon-star">
+                  <use xlinkHref="/sprite.svg#icon-star" />
+                </svg>
+                <svg className="overview__icon-star">
+                  <use xlinkHref="/sprite.svg#icon-star" />
+                </svg>
+              </div>
+              <div className="overview__location">
+                <svg className="overview__icon-location">
+                  <use xlinkHref="/sprite.svg#icon-location-pin" />
+                </svg>
+                <button className="btn-inline">Bolivia</button>
+              </div>
+              <div className="overview__rating">
+                <div className="overview__rating-average">8.6</div>
+                <div className="overview__rating-count">859 Votes</div>
+              </div>
+            </div>
+          </main>
         </div>
       </div>
     );
