463fa8ea78d43615f49c1568da51c2966cbce631
adamGuinea <adamguinea22@gmail.com>
2019-05-29 23:02:46 +1200

    wrote recommendations section

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,6 +4,9 @@ import Person1 from "../images/person-2.jpg";
 import Cycle1 from "../images/cycle-1.jpg";
 import Cycle2 from "../images/cycle-2.jpg";
 import Cycle3 from "../images/cycle-3.jpg";
+import Friend1 from "../images/friend-1.jpg";
+import Friend2 from "../images/friend-2.jpg";
+import Friend3 from "../images/friend-3.jpg";
 import { Link } from "react-router-dom";
 import MessageTimeline from "./MessageTimeline";
 
@@ -128,6 +131,69 @@ const Homepage = ({ currentUser }) => {
                 <div className="overview__rating-average">8.6</div>
                 <div className="overview__rating-count">859 Votes</div>
               </div>
+            </div>
+
+            <div className="detail">
+              <div className="description">
+                <p className="paragraph">
+                  Our tour company will pick you up at any of the main hostels
+                  or hotels, or you can meet at a predetermined point for a van
+                  ride up to the peak of La Cumbre where you will start your
+                  trek. The descent starts slow and mild, but savor the easy
+                  first portion and use it to get used to the handling of your
+                  bike, before the going gets steep and rough.
+                </p>
+                <p className="paragraph">
+                  The downhill portion of the trip lasts a full 64 kilometers
+                  (40 miles), with very few short uphills, ensuring that you’ll
+                  get tired not from pedaling, but from controlling your speed,
+                  taking sharp turns, and keeping your balance on thin roads
+                  above massive cliffs. The route will take you from the snow
+                  capped andean peaks, along winding mountainsides and cliffs,
+                  sometimes over a kilometer down, to the hot Amazon jungle down
+                  below.
+                </p>
+                <ul className="list">
+                  <li className="list__item">Safety gear provided </li>
+                  <li className="list__item">Standby ambulance everyday</li>
+                  <li className="list__item">Knowledgeable local guides </li>
+                  <li className="list__item">Dual suspension bikes</li>
+                  <li className="list__item">
+                    Bikes serviced after every ride
+                  </li>
+                  <li className="list__item">Includes buffet meal</li>
+                  <li className="list__item">Pickup from your hotel</li>
+                  <li className="list__item">Regular photo stops</li>
+                </ul>
+                <div className="recommend">
+                  <p className="recommend__count">
+                    Adam and 3 other friends recommend this tour.
+                  </p>
+                  <div className="recommend__friends">
+                    <img
+                      src={Person1}
+                      alt="friend 1"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={Friend1}
+                      alt="friend 2"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={Friend2}
+                      alt="friend 3"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={Friend3}
+                      alt="friend 4"
+                      className="recommend__photo"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="user-reviews">user reviews</div>
             </div>
           </main>
         </div>
